739d6f0e4d99397fdeefb97c1270fc16e167274a
gzkdev <ezihegodswill01@gmail.com>
2022-10-10 13:54:31 +0100

    chore: add image assets

---
+++ src/assets/index.tsx
@@ -0,0 +1,31 @@
+import tcIMG1 from "./Rectangle 17.png";
+import tcIMG2 from "./Rectangle 17-1.png";
+import tcIMG3 from "./Rectangle 17-2.png";
+
+import aIMG1 from "./Rectangle 14-1.png";
+import aIMG2 from "./Rectangle 14-2.png";
+import aIMG3 from "./Rectangle 14-3.png";
+import aIMG4 from "./Rectangle 14-4.png";
+import aIMG5 from "./Rectangle 14-5.png";
+import aIMG6 from "./Rectangle 14-6.png";
+import aIMG7 from "./Rectangle 14-7.png";
+
+import HeroIMG from "./Pexels Photo by Eric Esma.png";
+import heroSVG from "./Vector.svg";
+import Logo from "./logo.svg";
+
+export {
+  tcIMG1,
+  tcIMG2,
+  tcIMG3,
+  aIMG1,
+  aIMG2,
+  aIMG3,
+  aIMG4,
+  aIMG5,
+  aIMG6,
+  aIMG7,
+  HeroIMG,
+  heroSVG,
+  Logo,
+};
